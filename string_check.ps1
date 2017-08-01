693302fe843d0a610277c56ceca2da5e04cff626
davidmpires <davidmpires@yahoo.com>
2017-08-01 23:45:10 +0200

    string_check
    Checks for a sucessfull string on the verification .txt

---
+++ string_check.ps1
@@ -0,0 +1,21 @@
+﻿$date = Get-Date
+$appVersion = "10.3"
+$bin_location    = "C:\Program Files\Tableau\Tableau Server\" + $appVersion + "\bin"
+$backups_folder  = "C:\Backup\"
+$backups_file          = "tabsvc_"+ $date.Year+$date.Month+$date.Day
+$tabadmin = $bin_location + "\tabadmin.exe"
+$strFileName = $backups_folder+$backups_file+".tsbak"
+
+#Query file to find string
+
+$Text = Select-String -Path C:\Backup\verification.txt  -Pattern "===== Verify database completed successfully."
+
+ IF($Text -ne $null) 
+ {
+  Write-Host "Sucess"
+  exit 0
+ }
+ ELSE{
+ Write-Host "Error reading the string"
+ exit 20
+ }
