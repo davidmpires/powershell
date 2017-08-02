2a93c679961509a007bc668b2e555a949f3ac990
davidmpires <davidmpires@yahoo.com>
2017-08-02 09:27:23 +0200

    verify_backup
    Checks backup file exists, runs verify_database on the backup file, exports result to a txt and searches for sucessful string.

---
+++ verify_backup.ps1
@@ -0,0 +1,34 @@
+﻿$date = Get-Date
+$appVersion = "10.3"
+$bin_location    = "C:\Program Files\Tableau\Tableau Server\" + $appVersion + "\bin"
+$backups_folder  = "C:\Backup\"
+$backups_file          = "tabsvc_"+ $date.Year+$date.Month+$date.Day
+$tabadmin = $bin_location + "\tabadmin.exe"
+$strFileName = $backups_folder+$backups_file+".tsbak"
+$file = "C:\Backup\verification.txt"
+$Text = Select-String -Path C:\Backup\verification.txt  -Pattern "===== Verify database completed successfully."
+
+
+#Run backup verification to a txt file
+
+
+IF (Test-Path $strFileName)
+{
+& "$tabadmin" verify_database -f $strFileName | Out-File $file
+Write-Host "File Found"
+    IF($Text -ne $null)
+    {
+    Write-Host "String Checked"
+    exit 0
+    }
+    ELSE
+    {
+    Write-Host "Error reading the string"
+    exit 20
+    }
+}
+ELSE
+{Write-Host "File Not Found"
+exit 20
+}
+
